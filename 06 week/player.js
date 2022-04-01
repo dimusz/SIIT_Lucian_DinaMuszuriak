f6e29f1cef2d58f11d8992b68378196faf7155e0
dimusz <dina.lucian@yahoo.com>
2022-04-02 01:34:14 +0300

    update

--- 06 week/player.js
+++ 06 week/player.js
@@ -1,29 +1,55 @@
-let player = document.querySelector('.player')
+let player = document.querySelector(".player");
 
-document.addEventListener('keydown', function(event){
-    if (event.code === 'ArrowDown'){
-        let oldTop = parseInt(player.style.top)
-        if (oldTop < 370) {
-            player.style.top = `${oldTop + 10}px`
-          }
-        
-    } 
-    if (event.code === "ArrowRight") {
-        let oldLeft = parseInt(player.style.left) 
-        if (oldLeft < 370) {
-          player.style.left = `${oldLeft + 10}px` 
-        }
-      }
-    if(event.code === 'ArrowLeft'){
-        let oldLeft = parseInt(player.style.left)
-        if (oldLeft >= 10) {
-            player.style.left = `${oldLeft -10}px`
-        }
+document.addEventListener("keydown", function (event) {
+  if (event.code === "ArrowDown") {
+    let oldTop = parseInt(player.style.top);
+    if (oldTop < 350) {
+      player.style.top = `${oldTop + 10}px`;
     }
-    if(event.code === 'ArrowUp'){
-        let oldTop = parseInt(player.style.top)
-        if (oldTop >= 10) {
-            player.style.top = `${oldTop -10}px`
-        }
+  }
+  if (event.code === "ArrowRight") {
+    let oldLeft = parseInt(player.style.left);
+    if (oldLeft < 350) {
+      player.style.left = `${oldLeft + 10}px`;
     }
-    });
+  }
+  if (event.code === "ArrowLeft") {
+    let oldLeft = parseInt(player.style.left);
+    if (oldLeft >= 10) {
+      player.style.left = `${oldLeft - 10}px`;
+    }
+  }
+  if (event.code === "ArrowUp") {
+    let oldTop = parseInt(player.style.top);
+    if (oldTop >= 10) {
+      player.style.top = `${oldTop - 10}px`;
+    }
+  }
+});
+
+let playBox = document.querySelector(".play-box");
+let container = document.querySelector(".container");
+
+function obstacole() {
+  let obstacle = document.createElement("div");
+  obstacle.className = "obstacle";
+  obstacle.style.top = `${Math.random() * 350}px`;
+  obstacle.style.left = `${Math.random() * 350}px`;
+
+  playBox.appendChild(obstacle);
+}
+obstacole();
+obstacole();
+obstacole();
+obstacole();
+
+function createLives() {
+  let lives = document.createElement("div");
+  lives.className = "lives";
+
+  container.appendChild(lives);
+}
+createLives();
+createLives();
+createLives();
+
